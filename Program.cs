3399546e374c7b0237dc22cbc2f2f7c7fad2c563
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 17:52:46 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+int i=Convert.ToInt32(Console.ReadLine());
+int i1=i/100;
+int i2=i/10;
+int i3=i2-(i1*10);
+Console.WriteLine(i3);
