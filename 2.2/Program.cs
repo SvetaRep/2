632777b099dbc882bd3f693210d1498d62f4022c
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 18:03:19 +0300

    done

---
+++ 2.2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int i=Convert.ToInt32(Console.ReadLine());
+if (i<=5) Console.WriteLine("Это будний день.");
+else Console.WriteLine("Это выходной день.");
