a4d4ba9fd5dd8ab525c8420afce72c2d5dc3d3ee
Svetlana <svetarepalenko@yandex.ru>
2022-08-25 18:00:53 +0300

    done

---
+++ 2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1;
+Console.WriteLine("Введите число");
+num1 = Convert.ToInt32(Console.ReadLine());
+string name = num1.ToString();
+char[] charArray = name.ToCharArray();
+ if (num1>99) 
+     {
+    char first = charArray[2]; 
+     Console.WriteLine(first);
+     }
+ else Console.WriteLine("Третье число отсутствует.");
